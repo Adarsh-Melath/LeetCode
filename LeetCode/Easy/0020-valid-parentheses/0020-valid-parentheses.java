8d1825063bfeef3dece4956b85bcfd1163c3ba96
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-03-23 13:13:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -12,4 +12,24 @@ class Solution {
 
         return s.isEmpty();
     }
+
+    public static boolean optimal(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> closeToOpen = new HashMap<>();
+
+        closeToOpen.put(')', '(');
+        closeToOpen.put(']', '[');
+        closeToOpen.put('{', '}');
+        for (char c : s.toCharArray()) {
+            if (closeToOpen.containsKey(c)) {
+                if (!stack.isEmpty() && stack.peek() == closeToOpen.get(c))
+                    stack.pop();
+                else
+                    return false;
+            } else
+                stack.push(c);
+
+        }
+        return stack.isEmpty();
+    }
 }
