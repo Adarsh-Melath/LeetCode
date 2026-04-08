af01bf231ee99233a4d62cff2a4c084b57ea9bb7
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-04-08 13:15:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -3,17 +3,27 @@ class Solution {
         return solution(nums);
     }
 
-    public static int solution(int []nums)
-    {
-        int index=0;
-        for(int i=1;i<nums.length;i++)
-        {
-            if(nums[i-1]>nums[i])
-            {
-                index=i;
-                break;
+   public static int solution(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = (low + high) / 2;
+        int min1 = binarysearch(0, mid, nums);
+        int min2 = binarysearch(mid + 1, high, nums);
+        return Math.min(min1, min2);
+    }
+
+    public static int binarysearch(int low, int high, int[] nums) {
+        int minimum = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                minimum = nums[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return nums[index];
+
+        return minimum;
     }
 }
