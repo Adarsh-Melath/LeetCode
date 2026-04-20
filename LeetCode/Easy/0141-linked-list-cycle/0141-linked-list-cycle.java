c0c77cd273177096f23af3ca0a0e917bf6e756e8
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-04-20 10:16:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,16 +13,31 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
 
         //brute 
-        Map<ListNode, Integer> linkMap = new HashMap<>();
-        ListNode current = head;
+        // Map<ListNode, Integer> linkMap = new HashMap<>();
+        // ListNode current = head;
 
-        while (current != null) {
-            if (linkMap.containsKey(current)) {
-                return true;
-            }
+        // while (current != null) {
+        //     if (linkMap.containsKey(current)) {
+        //         return true;
+        //     }
 
-            linkMap.put(current,current.val);
-            current=current.next;
+        //     linkMap.put(current,current.val);
+        //     current=current.next;
+        // }
+
+        // return false;
+
+
+        //optimal   
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next !=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast)return true;
         }
 
         return false;
