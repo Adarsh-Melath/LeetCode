6b3442f31ad9111a96e0dac50137d9f375f0ec0c
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-04-23 22:04:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,69 +14,33 @@ class Node {
 */
 
 class Solution {
-    // public Node copyRandomList(Node head) {
-    //    Map<Node,Node> oldToNewMap=new HashMap<>();
+    public Node copyRandomList(Node head) {
+       Map<Node,Node> oldToNewMap=new HashMap<>();
 
-    //    Node dummyNode=new Node(0);
-    //    Node current=dummyNode;
+       Node dummyNode=new Node(0);
+       Node current=dummyNode;
 
-    //     // First pass: Create all nodes and build the next pointers
-    //     // Also populate the mapping from original to copied nodes
-    //     Node currentOriginal=head;
-    //     while(currentOriginal!=null)
-    //     {
-    //         Node newNode=new Node(currentOriginal.val);
-
-    //         current.next=newNode;
-    //         current=newNode;
-    //         oldToNewMap.put(currentOriginal,newNode);
-    //         currentOriginal=currentOriginal.next;
-    //     }
-
-    //     currentOriginal=head;
-    //     while(currentOriginal!=null)
-    //     {
-    //         Node copyNode=oldToNewMap.get(currentOriginal);
-
-    //         copyNode.random=(currentOriginal.random==null)?null : oldToNewMap.get(currentOriginal);
-    //         currentOriginal=currentOriginal.next;
-    //     }
-    //     return dummyNode.next;
-    // }
-
-     public Node copyRandomList(Node head) {
-        // HashMap to store mapping from original nodes to copied nodes
-        Map<Node, Node> oldToNewMap = new HashMap<>();
-      
-        // Create a dummy node to simplify list construction
-        Node dummyHead = new Node(0);
-        Node currentTail = dummyHead;
-      
         // First pass: Create all nodes and build the next pointers
         // Also populate the mapping from original to copied nodes
-        for (Node currentOriginal = head; currentOriginal != null; currentOriginal = currentOriginal.next) {
-            // Create a new node with the same value as the original
-            Node newNode = new Node(currentOriginal.val);
-          
-            // Connect the new node to the copied list
-            currentTail.next = newNode;
-            currentTail = newNode;
-          
-            // Store the mapping from original node to copied node
-            oldToNewMap.put(currentOriginal, newNode);
+        Node currentOriginal=head;
+        while(currentOriginal!=null)
+        {
+            Node newNode=new Node(currentOriginal.val);
+
+            current.next=newNode;
+            current=newNode;
+            oldToNewMap.put(currentOriginal,newNode);
+            currentOriginal=currentOriginal.next;
         }
-      
-        // Second pass: Set the random pointers for all copied nodes
-        for (Node currentOriginal = head; currentOriginal != null; currentOriginal = currentOriginal.next) {
-            // Get the corresponding copied node
-            Node copiedNode = oldToNewMap.get(currentOriginal);
-          
-            // Set the random pointer of the copied node
-            // If original's random is null, set to null; otherwise, get the corresponding copied node
-            copiedNode.random = (currentOriginal.random == null) ? null : oldToNewMap.get(currentOriginal.random);
+
+        currentOriginal=head;
+        while(currentOriginal!=null)
+        {
+            Node copyNode=oldToNewMap.get(currentOriginal);
+
+            copyNode.random=(currentOriginal.random==null)?null : oldToNewMap.get(currentOriginal.random);
+            currentOriginal=currentOriginal.next;
         }
-      
-        // Return the head of the copied list (skip dummy node)
-        return dummyHead.next;
+        return dummyNode.next;
     }
 }
