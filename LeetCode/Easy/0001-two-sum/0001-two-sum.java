c44d8969b840f435005783a61c3c29045876aee3
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-04-29 20:50:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,17 +1,35 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int i=0;i<nums.length;i++) {
-            int diff = target - nums[i];
-
-            if (map.containsKey(diff)) {
-                return new int[]{map.get(diff),i};
-            }
-
-            map.put(nums[i],i);
+        Pair[] arr = new Pair[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = new Pair(nums[i], i);
         }
 
-        return new int[]{-1,-1};
+        Arrays.sort(arr, (a, b) -> a.val - b.val);
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left].val + arr[right].val;
+
+            if (sum == target) {
+                return new int[] { arr[left].index, arr[right].index };
+            } else if (sum > target)
+                right--;
+            else
+                left++;
+        }
+        return new int[] { -1, -1 };
+    }
+}
+
+class Pair {
+    int val;
+    int index;
+
+    public Pair(int val, int index) {
+        this.val = val;
+        this.index = index;
     }
 }
