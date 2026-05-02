12bc8b4e775e2c0e24fba2a8954f491ae5f99870
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-02 07:41:11 +0530

    Time: 2 ms (99.32%), Space: 47.2 MB (40.36%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,35 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Pair[] arr = new Pair[nums.length];
+
+        //optimal
+        Map<Integer, Integer> map = new HashMap<>();
+
         for (int i = 0; i < nums.length; i++) {
-            arr[i] = new Pair(nums[i], i);
+            int compliment = target - nums[i];
+
+            if (map.containsKey(compliment)) {
+                return new int[] { map.get(compliment), i };
+            }
+
+            map.put(nums[i], i);
         }
 
-        Arrays.sort(arr, (a, b) -> a.val - b.val);
-
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left < right) {
-            int sum = arr[left].val + arr[right].val;
-
-            if (sum == target) {
-                return new int[] { arr[left].index, arr[right].index };
-            } else if (sum > target)
-                right--;
-            else
-                left++;
-        }
-        return new int[] { -1, -1 };
-    }
-}
-
-class Pair {
-    int val;
-    int index;
-
-    public Pair(int val, int index) {
-        this.val = val;
-        this.index = index;
+        return new int[]{-1,-1};
     }
 }
