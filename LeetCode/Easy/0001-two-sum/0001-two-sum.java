29159352d789a1bf439c0b2b4dc5f452bfd6744f
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-02 07:51:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,19 +1,51 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        //optimal
-        Map<Integer, Integer> map = new HashMap<>();
+        //Two sum brute
+        Pair[] arr = new Pair[nums.length];
 
         for (int i = 0; i < nums.length; i++) {
-            int compliment = target - nums[i];
+            arr[i] = new Pair(nums[i], i);
+        }
 
-            if (map.containsKey(compliment)) {
-                return new int[] { map.get(compliment), i };
-            }
+        Arrays.sort(arr,(a,b)->a.value-b.value);
 
-            map.put(nums[i], i);
+        int left=0;
+        int right=arr.length-1;
+
+        while(left<right){
+            int sum=arr[left].value+arr[right].value;
+
+            if(sum==target)return new int[]{arr[left].index,arr[right].index};
+            else if(sum>target)right--;
+            else left++;
         }
 
         return new int[]{-1,-1};
+
+        //optimal
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     int compliment = target - nums[i];
+
+        //     if (map.containsKey(compliment)) {
+        //         return new int[] { map.get(compliment), i };
+        //     }
+
+        //     map.put(nums[i], i);
+        // }
+
+        // return new int[]{-1,-1};
+    }
+}
+
+class Pair {
+    int value;
+    int index;
+
+    public Pair(int value, int index) {
+        this.value = value;
+        this.index = index;
     }
 }
