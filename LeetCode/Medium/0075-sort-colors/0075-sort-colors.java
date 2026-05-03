ec5120931133d05a2c95a6348896a5820e239c42
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-03 18:01:46 +0530

    Time: 0 ms (100%), Space: 43.5 MB (59.94%) - LeetHub

--- LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
+++ LeetCode/Medium/0075-sort-colors/0075-sort-colors.java
@@ -1,57 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
 
-        //Dutch National flag
-        int low=0;
-        int high=nums.length-1;
-        int mid=0;
-
-        while(mid<=high)
-        {
-            if(nums[mid]==2)
-            {
-                int temp=nums[mid];
-                nums[mid]=nums[high];
-                nums[high]=temp;
+        while (mid <= high) {
+            if (nums[mid] == 2) {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
                 high--;
-            }else if(nums[mid]==1)mid++;
-            else {
-                int temp=nums[mid];
-                nums[mid]=nums[low];
-                nums[low]=temp;
-
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
                 low++;
                 mid++;
             }
         }
-
-        //count number specifically  but break the constraint
-        // int count0 = 0;
-        // int count1 = 0;
-        // int count2 = 0;
-
-        // for (int num : nums) {
-        //     if (num == 0) {
-        //         count0++;
-        //     } else if (num == 1)
-        //         count1++;
-        //     else count2++;
-        // }
-
-        // System.out.println(count0);
-        // System.out.println(count1);
-        // System.out.println(count2);
-
-        // for(int i=0;i<count0;i++)
-        //     nums[i]=0;
-        
-        // for(int i=count0;i<count0+count1;i++)
-        //     nums[i]=1;
-
-        // for(int i=count0+count1;i<nums.length;i++)
-        //     nums[i]=2;
-
-        //solution one but break the constraint 
-        //  Arrays.sort(nums);
     }
 }
