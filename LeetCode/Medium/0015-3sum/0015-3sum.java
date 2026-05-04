de5c8a944a52b34b53249748bcff565c4acd71de
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-04 07:02:51 +0530

    Time: 39 ms (20.62%), Space: 59.5 MB (21.71%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.java
+++ LeetCode/Medium/0015-3sum/0015-3sum.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        //two pointer approach(optimal)
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i - 1] == nums[i])
-                continue;
 
-            //two sum 
+        System.out.println(Arrays.toString(nums));
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
             int left = i + 1;
             int right = nums.length - 1;
+
             while (left < right) {
-                int sum = nums[i]+nums[left] + nums[right];
+                int sum = nums[i] + nums[right] + nums[left];
 
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
+                    right--;
                     while (left < right && nums[left] == nums[left - 1])
                         left++;
                 } else if (sum > 0)
@@ -27,58 +30,5 @@ class Solution {
         }
 
         return result;
-        // second approach
-        // HashMap<Integer, Integer> map = new HashMap<>();
-        // for (int num : nums) {
-        //     map.put(num, map.getOrDefault(num, 0) + 1);
-        // }
-
-        // System.out.println("Map : ");
-        // System.out.println(map);
-
-        // Set<List<Integer>> result = new HashSet<>();
-        // for (int i = 0; i < nums.length; i++) {
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         int pending = -(nums[i] + nums[j]);
-        //         int count =map.containsKey(pending)?map.get(pending):0;
-        //         if (pending == nums[i])
-        //             count--;
-        //         if (pending == nums[j])
-        //             count--;
-
-        //         if (count > 0) {
-        //             List<Integer> temp = new ArrayList<>();
-        //             temp.add(nums[i]);
-        //             temp.add(nums[j]);
-        //             temp.add(pending);
-
-        //             Collections.sort(temp);
-        //             result.add(temp);
-        //         }
-        //     }
-        // }
-        // return new ArrayList<>(result);
-
-        //brute force
-        // Set<List<Integer>> result = new HashSet<>();
-        // for (int i = 0; i < nums.length; i++) {
-        //     for (int j = i + 1; j < nums.length; j++) {
-        //         for (int k = j + 1; k < nums.length; k++) {
-        //             int sum = nums[i] + nums[j] + nums[k];
-
-        //             if (sum == 0) {
-        //                 List<Integer> temp = new ArrayList<>();
-
-        //                 temp.add(nums[i]);
-        //                 temp.add(nums[j]);
-        //                 temp.add(nums[k]);
-
-        //                 Collections.sort(temp);
-        //                 result.add(temp);
-        //             }
-        //         }
-        //     }
-        // }
-        // return new ArrayList<>(result);
     }
 }
