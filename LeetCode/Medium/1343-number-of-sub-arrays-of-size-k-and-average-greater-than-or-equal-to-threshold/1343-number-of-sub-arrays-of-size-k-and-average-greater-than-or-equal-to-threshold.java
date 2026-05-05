bf53834cae81a05f5ef6882d909ddeec0ebea32c
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-05 09:29:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ LeetCode/Medium/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,17 +1,38 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        //brute
-        int count=0;
-        for(int i=0;i<arr.length-k+1;i++){
-            int sum=0;
-            for(int j=i;j<i+k;j++){
-                sum+=arr[j];
-            }
+        //optimal
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int count = 0;
+        int avg = (int) sum / k;
 
-            int avg=(int) sum/k;
-            if(avg>=threshold)count++;
+        if (avg >= threshold)
+            count++;
+        int start = 0;
+        int end = k;
+
+        while (end < arr.length) {
+            sum-=arr[start++];
+            sum+=arr[end++];
+
+            count+=(sum)/k >=threshold ?1:0;
         }
 
         return count;
+        //brute
+        // int count=0;
+        // for(int i=0;i<arr.length-k+1;i++){
+        //     int sum=0;
+        //     for(int j=i;j<i+k;j++){
+        //         sum+=arr[j];
+        //     }
+
+        //     int avg=(int) sum/k;
+        //     if(avg>=threshold)count++;
+        // }
+
+        // return count;
     }
 }
