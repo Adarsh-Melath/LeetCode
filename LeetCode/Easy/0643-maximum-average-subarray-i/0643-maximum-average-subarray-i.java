ef6cf6c56eac11ad9e860b834fd1afca3dad7a4f
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-05 07:54:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ LeetCode/Easy/0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,48 +1,38 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
 
-        //brute
-        if(nums.length==1)return nums[0];
-        int maxSum=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length-k+1;i++){
-            int sum=0;
-            for(int j=i;j<i+k && j<nums.length;j++){
-                sum+=nums[j];
-            }
+        //optimal
+        int maxSum=0;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+
+        maxSum=sum;
+
+        int start=0;
+        int end=k;
+
+        while(end<nums.length){
+            sum-=nums[start++];
+            sum+=nums[end++];
+
             maxSum=Math.max(maxSum,sum);
         }
 
         return (double)maxSum/k;
+
+        //brute
         // if(nums.length==1)return nums[0];
-        // int left=0;
-        // int right=0;
-        // int sum=0;
-        // double avg=0;
-        // double maxAvg=0;
-
-        // while(right<nums.length){
-        //     while(right-left<k){
-        //         System.out.println("right-left : "+(right-left)+" K: "+k);
-        //         sum+=nums[right];
-        //         System.out.println("sum : "+sum);
-        //         right++;
-        //         System.out.println("right : "+right);
+        // int maxSum=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length-k+1;i++){
+        //     int sum=0;
+        //     for(int j=i;j<i+k && j<nums.length;j++){
+        //         sum+=nums[j];
         //     }
-
-        //     avg=(double)sum/k;
-        //     System.out.println("avg : "+avg);
-        //     maxAvg=Math.max(avg,maxAvg);
-        //     System.out.println("maxAverage: "+ maxAvg);
-
-        //     avg=(sum-nums[left])/(k-1);
-        //     System.out.println("sum-nums[left]/k-1: "+(sum-nums[left])/(k-1) );
-        //     System.out.println("avg : "+avg);
-        //     left++;
-        //     System.out.println("left : "+left );
-
-        //     System.out.println();
+        //     maxSum=Math.max(maxSum,sum);
         // }
 
-        // return maxAvg;
+        // return (double)maxSum/k;
     }
 }
