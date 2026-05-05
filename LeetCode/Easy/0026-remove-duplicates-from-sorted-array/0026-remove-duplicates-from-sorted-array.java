eaf8e57e39a469024f1932f2fd6ae802d9df54f5
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-05 06:20:27 +0530

    Time: 1 ms (76.59%), Space: 46.5 MB (80.33%) - LeetHub

--- LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,12 +3,13 @@ class Solution {
         int left=1;
         for(int right=1;right<nums.length;right++){
             if(nums[right]!=nums[right-1]){
+               
                 nums[left]=nums[right];
+
                 left++;
             }
         }
 
         return left;
-
     }
 }
