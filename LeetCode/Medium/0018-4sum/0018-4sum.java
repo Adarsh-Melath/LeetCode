167fd293b013707c38bfe1e8b984ba0a2f5bf774
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-09 09:49:21 +0530

    Time: 19 ms (81.94%), Space: 45.5 MB (87.03%) - LeetHub

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -1,11 +1,12 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> result=new ArrayList<>();
+
         Arrays.sort(nums);
         for(int i=0;i<nums.length-3;i++){
-            if(i>0 && nums[i]==nums[i-1])continue;
+            if(i >  0 && nums[i-1] == nums[i])continue;
             for(int j=i+1;j<nums.length-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1])continue;
+                if(j>i+1 && nums[j-1]==nums[j])continue;
 
                 int left=j+1;
                 int right=nums.length-1;
@@ -13,19 +14,20 @@ class Solution {
                 while(left<right){
                     long sum=(long)nums[i]+nums[j]+nums[left]+nums[right];
 
-                    if(sum>target)right--;
-                    else if(sum<target)left++;
-                    else {
+                    if(sum==target)
+                    {
                         result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
+
                         left++;
                         right--;
 
-                        while(left<right && nums[left]==nums[left-1])left++;                
+                        while(left<right && nums[left]==nums[left-1])left++;
                     }
+                    else if(sum>target)right--;
+                    else left++;
                 }
             }
         }
-
-        return result;  
+        return result;
     }
 }
