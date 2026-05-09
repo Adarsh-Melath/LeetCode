873cd07aa577e569d306d1c26c98b03bb366f75a
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-09 10:04:23 +0530

    Time: 1 ms (59.1%), Space: 47.5 MB (80.53%) - LeetHub

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,22 @@
 class Solution {
     public int trap(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-        int leftMax=nums[left];
-        int rightMax=nums[right];
-        int sum=0;
+        int sum = 0;
+        int left = 0;
+        int right = nums.length - 1;
+        int maxLeft = nums[left];
+        int maxRight = nums[right];
 
-        while(left<right){
-            if(nums[left]<nums[right]){
+        while (left < right) {
+            if (nums[left] < nums[right]) {
                 left++;
-                leftMax=Math.max(nums[left],leftMax);
-                sum+=leftMax-nums[left];
-            }else{
+                maxLeft = Math.max(maxLeft, nums[left]);
+                sum += maxLeft - nums[left];
+            } else {
                 right--;
-                rightMax=Math.max(nums[right],rightMax);
-                sum+=rightMax-nums[right];
+                maxRight = Math.max(maxRight, nums[right]);
+                sum += maxRight - nums[right];
             }
         }
-
-        return sum ;
+        return sum;
     }
 }
