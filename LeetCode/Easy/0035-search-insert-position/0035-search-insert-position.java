a2aa34c10673f49626419bb673e45ceda0387954
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-09 19:04:36 +0530

    Time: 0 ms (100%), Space: 44.8 MB (47.43%) - LeetHub

--- LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
+++ LeetCode/Easy/0035-search-insert-position/0035-search-insert-position.java
@@ -3,20 +3,16 @@ class Solution {
 
         int left = 0;
         int right = nums.length - 1;
-        int position = -1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                position = mid;
                 return mid;
             } else if (nums[mid] > target) {
-                position = mid; 
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return position  !=-1 ? position : left;
+        return left;
     }
 }
