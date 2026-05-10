a7151e1a1d7910d2d06cddf0acf91100bfe192a9
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-10 10:03:42 +0530

    Time: 2 ms (100%), Space: 72.1 MB (7.93%) - LeetHub

--- LeetCode/Medium/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ LeetCode/Medium/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -2,12 +2,15 @@ class Solution {
     public int numOfSubarrays(int[] nums, int k, int threshold) {
         int sum = 0;
         int count = 0;
+
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
 
-        if (sum >= (k * threshold))
+        int condition = k * threshold;
+        if (sum >= (condition)) {
             count++;
+        }
 
         int left = 0;
         int right = k;
@@ -15,9 +18,13 @@ class Solution {
         while (right < nums.length) {
             sum = sum - nums[left] + nums[right];
 
-            if(sum>=(k*threshold))count++;
-            right++;left++;
+            if (sum >= condition) {
+                count++;
+            }
+
+            left++;
+            right++;
         }
-return count;
+        return  count;
     }
 }
