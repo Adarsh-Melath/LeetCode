dc672211650c83883f04cd8a386c12fd8e6ea119
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-10 20:21:16 +0530

    Time: 1 ms (59.12%), Space: 48.7 MB (15.71%) - LeetHub

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,50 @@
 class Solution {
     public int trap(int[] nums) {
+        //optimal 2: 
         int sum = 0;
-        int left = 0;
-        int right = nums.length - 1;
-        int maxLeft = nums[left];
-        int maxRight = nums[right];
+        int[] maxLeft = new int[nums.length];
+        int[] maxRight=new int [nums.length];
 
-        while (left < right) {
-            if (nums[left] < nums[right]) {
-                left++;
-                maxLeft = Math.max(maxLeft, nums[left]);
-                sum += maxLeft - nums[left];
-            } else {
-                right--;
-                maxRight = Math.max(maxRight, nums[right]);
-                sum += maxRight - nums[right];
-            }
+        int maxL=0;
+        int maxR=0;
+
+        for(int i=0;i<nums.length;i++){
+            maxLeft[i]=maxL;
+            maxL=Math.max(maxL,nums[i]);
         }
+
+        for(int i=nums.length-1;i>=0;i--){
+            maxRight[i]=maxR;
+            maxR=Math.max(maxR,nums[i]);
+        }
+
+
+for(int i=0;i<nums.length;i++){
+  int waterLevel=Math.min(maxLeft[i],maxRight[i]);
+
+  if(waterLevel>nums[i]){
+    sum+=waterLevel-nums[i];
+  }
+}
         return sum;
+        //optimal 1: 
+        // int sum = 0;
+        // int left = 0;
+        // int right = nums.length - 1;
+        // int maxLeft = nums[left];
+        // int maxRight = nums[right];
+
+        // while (left < right) {
+        //     if (nums[left] < nums[right]) {
+        //         left++;
+        //         maxLeft = Math.max(maxLeft, nums[left]);
+        //         sum += maxLeft - nums[left];
+        //     } else {
+        //         right--;
+        //         maxRight = Math.max(maxRight, nums[right]);
+        //         sum += maxRight - nums[right];
+        //     }
+        // }
+        // return sum;
     }
 }
