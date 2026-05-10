55d3da2a90b75fd9d816ffca427d08f46ee02685
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-10 23:12:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (80.86%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,31 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int mid = (nums.length / 2);
-        if (nums[0] > target) {
-            return binarySearch(nums, mid+1, nums.length-1, target);
-        } else if (nums[0] < nums[nums.length-1])
-            return binarySearch(nums, 0, nums.length-1, target);
-        else if(nums[nums.length-1]<2){
-            return binarySearch(nums,0,mid,target);
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target)return mid;
+            
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>target){
+                    right=mid-1;
+                }else left=mid+1;
+            }else {
+                if(target > nums[mid] && nums[right]>=target)left=mid+1;
+                else right=mid-1;
+            }
         }
-        return -1;
-    }
-
-    public int binarySearch(int[] nums, int start, int end, int target) {
-        int left = start;
-        int right = end;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else
-                left = mid + 1;
-        }
-
         return -1;
     }
 }
