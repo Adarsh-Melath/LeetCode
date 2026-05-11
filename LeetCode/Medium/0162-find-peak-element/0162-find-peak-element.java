e82f558939109a045c80b9e5669661685c6bd329
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-11 09:57:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
+++ LeetCode/Medium/0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,15 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if(nums.length==1)return 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] > Integer.MIN_VALUE && nums[i] > nums[i + 1])
-                    return 0;
-            } else if (i == nums.length - 1) {
-                if (nums[i] > nums[i - 1] && nums[i] > Integer.MIN_VALUE)
-                    return i;
-            } else {
-                if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
-                    return i;
-            }
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<right){
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]<nums[mid+1])left=mid+1;
+            else right=mid;
         }
 
-        return -1;
+        return left;
     }
 }
