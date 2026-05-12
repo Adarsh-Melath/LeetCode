ca53afaef8f9ffecbd267aad5e12517df3e1a46b
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-12 18:58:41 +0530

    Time: 0 ms (100%), Space: 53.1 MB (8.54%) - LeetHub

--- LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ LeetCode/Medium/0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,16 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int left = 0;
+        int right = nums.length - 1;
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid % 2 == 1)
+                mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid - 1;
+            }
         }
 
-        for (int num : nums) {
-            if (map.get(num) == 1)
-                return num;
-        }
-
-        return -1;
+        return nums[left];
     }
 }
