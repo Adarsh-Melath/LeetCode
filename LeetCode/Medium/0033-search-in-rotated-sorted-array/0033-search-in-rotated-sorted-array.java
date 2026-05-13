3aa2a74c342496b317b7a20ce9c316a2b5fde81a
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-13 18:14:42 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.58%) - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -7,16 +7,20 @@ class Solution {
             int mid=left+(right-left)/2;
 
             if(nums[mid]==target)return mid;
-            
+
             if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && nums[mid]>target){
+                if(nums[left]<=target && target<nums[mid]){
                     right=mid-1;
-                }else left=mid+1;
-            }else {
-                if(target > nums[mid] && nums[right]>=target)left=mid+1;
-                else right=mid-1;
+                }
+                else left=mid+1;
             }
+            else 
+                if(nums[mid]<target && nums[right]>=target){
+                    left=mid+1;
+                }
+                else right=mid-1;
         }
+
         return -1;
     }
 }
