26c0f6f1f86c0695c950da67e6d7692e92e13221
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-13 09:32:25 +0530

    Time: 0 ms (100%), Space: 44 MB (42.42%) - LeetHub

--- LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ LeetCode/Medium/0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,33 +1,24 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return solution(matrix,target);        
+        for(int i=0;i<matrix.length;i++){
+          if( binarySearch(matrix,i,matrix[i].length,target))return true;
+        }
+
+        return false; 
     }
 
-    public static boolean solution(int [][]matrix,int target)
-    {
-        for(int[] row :matrix )
-        {
-            if(binarySearch(row,target))
-                return true;
+    public boolean binarySearch(int[][]matrix,int i,int j,int target){
+        
+        int left=0;
+        int right=j-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(matrix[i][mid]==target){return true;}
+            else if(target>matrix[i][mid])left=mid+1;
+            else right=mid-1;
         }
 
         return false;
     }
-
-    public static boolean binarySearch(int[] row,int target)
-    {
-        int low=0;
-        int high=row.length-1;
-
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(row[mid]==target)
-                return true;
-            else if(target>row[mid])
-                low=mid+1;
-            else high=mid-1;
-        }
-        return false;
-    }    
 }
