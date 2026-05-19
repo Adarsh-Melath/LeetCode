7b3136b219800e405992d9312e9d62301367839d
Adarsh-Melath <adarshmelath1305@gmail.com>
2026-05-19 20:22:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (58.97%) - LeetHub

--- LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
+++ LeetCode/Easy/0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,25 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        int last=m+n-1;
-
-        while(m>0 && n>0){
-            if(nums1[m-1]>nums2[n-1]){
-                nums1[last]=nums1[m-1];
-                m--;
-            }else{
-                nums1[last]=nums2[n-1];
-                n--;
-            }
-
-            last--;
-        }
-
-        while(n>0){
-            nums1[last]=nums2[n-1];
-            n--;
-            last--;
-        }
         
+        int last=nums1.length-1;
+
+        int i=m-1;
+        int j=n-1;
+
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[last]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[last]=nums2[j];
+                j--;
+            }
+            last--;
+        }
+
+        while(j>=0){
+            nums1[last]=nums2[j];
+            j--;
+            last--;
+        }
     }
 }
