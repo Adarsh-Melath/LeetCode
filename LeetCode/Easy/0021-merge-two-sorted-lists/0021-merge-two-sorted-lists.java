0e6d0ffeb2137b39e427dd92215e38e99299a373
Adarsh Melath <adarshmelath1305@gmail.com>
2026-05-22 16:37:58 +0530

    Time: 0 ms (100%), Space: 44.2 MB (73.25%) - LeetHub

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -13,11 +13,11 @@ class Solution {
         ListNode dummy=new ListNode(-1);
         ListNode current=dummy;
 
-        while(list1 !=null && list2!=null){
+        while(list1!=null && list2!=null){
             if(list1.val<list2.val){
                 current.next=list1;
                 list1=list1.next;
-            }else {
+            }else{
                 current.next=list2;
                 list2=list2.next;
             }
@@ -25,7 +25,7 @@ class Solution {
             current=current.next;
         }
 
-        current.next= list1==null ? list2 :list1;
+        current.next=list1==null?list2:list1;
         return dummy.next;
     }
 }
