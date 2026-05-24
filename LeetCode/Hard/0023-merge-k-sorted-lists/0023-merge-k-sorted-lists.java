7f1dc7ec368de82341cbe16dd63d89758072c87e
Adarsh Melath <adarshmelath1305@gmail.com>
2026-05-24 18:41:00 +0530

    Time: 83 ms (9.5%), Space: 47.1 MB (5.41%) - LeetHub

--- LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,24 +10,36 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        List<Integer> arr=new ArrayList<>();
-
-        for(ListNode head:lists){
-            while(head!=null){
-                arr.add(head.val);
-                head=head.next;
-            }
+        //better
+        if (lists.length == 0) {
+            return null;
         }
 
-        Collections.sort(arr);
+        ListNode merged = lists[0];
 
+        for (int i = 1; i < lists.length; i++) {
+            merged=merge(merged,lists[i]);
+        }
+        return merged;
+    }
+
+    public ListNode merge(ListNode list1,ListNode list2){
         ListNode dummy=new ListNode(-1);
         ListNode current=dummy;
 
-        for(Integer num:arr){
-            current.next=new ListNode(num);
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                current.next=list1;
+                list1=list1.next;
+            }
+            else{
+                current.next=list2;
+                list2=list2.next;
+            }
             current=current.next;
         }
+
+        current.next=list1!=null?list1:list2;
         return dummy.next;
     }
 }
