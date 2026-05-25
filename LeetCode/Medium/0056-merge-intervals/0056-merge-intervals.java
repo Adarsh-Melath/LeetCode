2387aa997f32d7bb8e352c360b32b2e6464cc256
Adarsh Melath <adarshmelath1305@gmail.com>
2026-05-25 09:26:26 +0530

    Time: 8 ms (90.69%), Space: 48.7 MB (89.97%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        int[][] output = new int[intervals.length][];
+        output[0] = intervals[0];
 
-        int [][]output=new int[intervals.length][2];
-        int index=0;
-
-        output[index]=intervals[index];
+        int index = 0;
 
         for(int i=1;i<intervals.length;i++){
             int lastEnd=output[index][1];
@@ -15,11 +14,12 @@ class Solution {
 
             if(start<=lastEnd){
                 output[index][1]=Math.max(lastEnd,end);
-            }else{
-                index++;
-                output[index]=intervals[i];
+            }
+            else{
+                output[++index]=intervals[i];
             }
         }
+
         return Arrays.copyOf(output,index+1);
     }
 }
