fc20718a5a63caf10f7bb5fab71f0cfb3f5efa76
Adarsh Melath <adarshmelath1305@gmail.com>
2026-06-08 08:37:16 +0530

    Time: 30 ms (42.49%), Space: 46.8 MB (68.42%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+
+        if (n == 1)
+            return 0;
+
+        DSU dsu = new DSU(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    dsu.union(i, j);
+                }
+            }
+        }
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dsu.find(i) == i)
+                components++;
+        }
+
+        return n-components;
+    }
+}
+
+class DSU {
+    int[] parent;
+    int[] size;
+    int count = 0;
+
+    public DSU(int n) {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] == x)
+            return x;
+
+        parent[x] = find(parent[x]);
+
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if (rootX == rootY)
+            return;
+
+        if (size[rootX] < size[rootY]) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
+        } else {
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+    }
+
+}
