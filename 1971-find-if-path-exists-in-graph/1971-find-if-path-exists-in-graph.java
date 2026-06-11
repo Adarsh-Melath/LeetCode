3420bda7923b4e702f5f50b46e4ce0a988deb83e
Adarsh Melath <adarshmelath1305@gmail.com>
2026-06-11 16:45:27 +0530

    Time: 24 ms (91.88%), Space: 253.6 MB (90.67%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,53 +1,52 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         DSU dsu = new DSU(n);
-
         for (int[] edge : edges) {
             dsu.union(edge[0], edge[1]);
         }
-
         return dsu.find(source) == dsu.find(destination);
     }
 }
 
 class DSU {
-    int parent[];
+    int[] parent;
     int[] size;
 
     public DSU(int n) {
         parent = new int[n];
         size = new int[n];
 
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=i;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
 
-    public int find(int x) {
-        if (parent[x] == x) {
+    public int find(int x){
+        if(parent[x]==x)
+        {
             return x;
         }
 
-        parent[x] = find(parent[x]);
-
+        parent[x]=find(parent[x]);
         return parent[x];
     }
 
     public void union(int x, int y) {
-        int rootOfX = find(x);
-        int rootOfY = find(y);
+        int rootX = find(x);
+        int rootY = find(y);
 
-        if (rootOfX == rootOfY) {
+        if (rootX == rootY) {
             return;
         }
 
-        if (size[rootOfX] < size[rootOfY]) {
-            parent[rootOfX] = rootOfY;
-            size[rootOfY] += size[rootOfX];
+        if (size[rootX] > size[rootY]) {
+            parent[rootX] = rootY;
+            size[rootY] += size[rootX];
         } else {
-            parent[rootOfY] = rootOfX;
-            size[rootOfX] += size[rootOfY];
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
         }
     }
+
 }
