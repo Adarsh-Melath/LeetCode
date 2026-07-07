568863d6d8ec5ac962a5610795fedcb2e2387734
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-07 12:24:47 +0530

    Time: 111 ms (56.01%), Space: 47 MB (52.49%) - LeetHub

--- LeetCode/Medium/0454-4sum-ii/0454-4sum-ii.java
+++ LeetCode/Medium/0454-4sum-ii/0454-4sum-ii.java
@@ -1,21 +1,17 @@
 class Solution {
     public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int count = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i : nums1) {
             for (int j : nums2) {
-                int sum = i + j;
-                map.put(sum, map.getOrDefault(sum, 0) + 1);
+                map.put((i + j), map.getOrDefault((i + j), 0) + 1);
             }
         }
 
+        int count = 0;
         for (int k : nums3) {
             for (int l : nums4) {
-                int sum = k + l;
-                if (map.containsKey(-sum)) {
-                    count += map.get(-sum);
-                }
+                count+=map.getOrDefault(-(k+l),0);
             }
         }
 
