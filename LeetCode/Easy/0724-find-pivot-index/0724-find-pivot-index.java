d7c47f42afc0cc564399ed3f9985321491af0969
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-11 04:59:46 +0530

    Time: 13 ms (9.39%), Space: 47.3 MB (77.96%) - LeetHub

---
+++ LeetCode/Easy/0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefix_sum = new int[nums.length];
+        int n = nums.length;
+        prefix_sum[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix_sum[i] = nums[i] + prefix_sum[i - 1];
+        }
+
+        System.out.println(Arrays.toString(prefix_sum));
+
+        for (int i = 0; i < n; i++) {
+            int left = i == 0 ? 0 : prefix_sum[i-1];
+            int right = i == n - 1 ? 0 : prefix_sum[n-1] - prefix_sum[i];
+
+            if (left == right) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
