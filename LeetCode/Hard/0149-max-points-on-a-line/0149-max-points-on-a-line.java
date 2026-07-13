f090ff5e223af1d71a9b4ed46b9793eaff9ab610
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-13 20:48:25 +0530

    Time: 42 ms (6.53%), Space: 46.2 MB (79.47%) - LeetHub

--- LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,49 +1,48 @@
 class Solution {
     public int maxPoints(int[][] points) {
+        int n = points.length;
         int answer = 1;
-        for (int i = 0; i < points.length; i++) {
+        for (int i = 0; i < n; i++) {
+            int maxF = 0;
             HashMap<String, Integer> map = new HashMap<>();
-            int maxFreq = 0;
-            for (int j = 0; j < points.length; j++) {
-                if (i == j)
-                    continue;
-
-                int dy = points[j][1] - points[i][1];
+            for (int j = 0; j < n; j++) {
+                if(i==j)continue;
                 int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
 
-                if (dy == 0) {
+                if (dy == 0)
                     dx = 1;
-                }
-
-                if (dx == 0) {
+                else if (dx == 0)
                     dy = 1;
+                else {
+                    if (dx < 0) {
+                        dx = -dx;
+                        dy = -dy;
+                    }
                 }
 
-                if (dx < 0) {
-                    dx = -dx;
-                    dy = -dy;
-                }
-
-                int gcd = gcd(Math.abs(dx), Math.abs(dy));
+                int gcd = gcd(Math.abs(dy), Math.abs(dx));
 
                 dx /= gcd;
                 dy /= gcd;
 
                 String key = dy + "/" + dx;
 
-                int freq = map.getOrDefault(key, 0) + 1;
-                map.put(key, freq);
-                maxFreq = Math.max(freq, maxFreq);
+                int f = map.getOrDefault(key, 0) + 1;
+                map.put(key,f);
+                maxF = Math.max(maxF, f);
+
             }
-            answer = Math.max(answer, maxFreq + 1);
+            answer = Math.max(answer, maxF + 1);
         }
+
         return answer;
     }
 
     public int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
-            b = a % b;
+            b = a% b;
             a = temp;
         }
         return a;
