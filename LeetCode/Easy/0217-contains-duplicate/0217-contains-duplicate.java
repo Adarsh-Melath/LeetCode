0dcf71ff9c1cfd09d6eb73d2801768a5ee6bb1a1
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-18 13:43:16 +0530

    Time: 9 ms (99.74%), Space: 104.7 MB (28.7%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>(n * 2);
         for (int num : nums) {
             if (!set.add(num))
                 return true;
