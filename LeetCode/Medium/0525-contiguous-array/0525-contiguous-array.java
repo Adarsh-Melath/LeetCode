40f34ac5eed7c8fecc406c57dc83c3b060281d14
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-18 16:48:18 +0530

    Time: 28 ms (8%), Space: 65.5 MB (59.2%) - LeetHub

--- LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
+++ LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
@@ -3,22 +3,23 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
         int prefix = 0;
-        int maxLength = 0;
+        int answer = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
+            if (nums[i] == 0) {
                 prefix--;
-            else
-                prefix++;
-
-            if (map.containsKey(prefix)) {
-                maxLength =Math.max( i - map.get(prefix),maxLength);
             } else {
-                map.put(prefix, i);
+                prefix++;
+            }
+
+            if(map.containsKey(prefix)){
+                int firstIndex=map.get(prefix);
+                answer=Math.max(answer,i-firstIndex);
+            }else{
+                map.put(prefix,i);
             }
         }
-        return maxLength;
+
+        return answer;
     }
 }
-
-//[0,1,1,0,1,1,1,0]
