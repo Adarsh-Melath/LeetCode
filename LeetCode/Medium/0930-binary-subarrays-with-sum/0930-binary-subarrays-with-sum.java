c8756a1684602db358bbd41a77c619753a330942
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-19 11:56:21 +0530

    Time: 23 ms (32.22%), Space: 50.4 MB (90.06%) - LeetHub

--- LeetCode/Medium/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ LeetCode/Medium/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
         map.put(0, 1);
+
         int prefix = 0;
         int answer = 0;
 
@@ -10,12 +10,9 @@ class Solution {
             prefix += num;
 
             int needed = prefix - goal;
-
             answer += map.getOrDefault(needed, 0);
-
             map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
-
         return answer;
     }
 }
