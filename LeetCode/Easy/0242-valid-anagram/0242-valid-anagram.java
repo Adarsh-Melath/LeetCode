58c913c0aa32ab9d30b65becc206f751e43ed09d
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-19 12:16:03 +0530

    Time: 4 ms (82.21%), Space: 44.5 MB (60.57%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        char[]sa=new char[26];
-        char[]ta=new char[26];
+        if (s.length() != t.length())
+            return false;
+        char[] characters = new char[26];
 
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            sa[ch-'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            characters[ch - 'a']++;
         }
-
-        System.out.println(Arrays.toString(sa));
 
         for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            ta[ch-'a']++;
+            characters[t.charAt(i)-'a']--;
         }
 
-        System.out.println(Arrays.toString(ta));
-
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(sa[ch-'a']!=ta[ch-'a'])return false;
+        for(int freq:characters){
+            if(freq!=0)return false;
         }
+
+
 
         return true;
     }
