3af8f667f959101371b5bcfeea33cac276117539
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-20 13:11:33 +0530

    Time: 48 ms (5.19%), Space: 46.6 MB (31.7%) - LeetHub

--- LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ LeetCode/Hard/0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,12 +1,12 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int n = points.length;
         int answer = 1;
-        for (int i = 0; i < n; i++) {
-            int maxF = 0;
+        for (int i = 0; i < points.length; i++) {
             HashMap<String, Integer> map = new HashMap<>();
-            for (int j = 0; j < n; j++) {
-                if(i==j)continue;
+            int max = 0;
+            for (int j = 0; j < points.length; j++) {
+                if (i == j)
+                    continue;
                 int dx = points[j][0] - points[i][0];
                 int dy = points[j][1] - points[i][1];
 
@@ -14,37 +14,30 @@ class Solution {
                     dx = 1;
                 else if (dx == 0)
                     dy = 1;
-                else {
-                    if (dx < 0) {
-                        dx = -dx;
-                        dy = -dy;
-                    }
+                if (dx < 0) {
+                    dy = -dy;
+                    dx = -dx;
                 }
 
                 int gcd = gcd(Math.abs(dy), Math.abs(dx));
-
-                dx /= gcd;
                 dy /= gcd;
-
+                dx /= gcd;
                 String key = dy + "/" + dx;
-
-                int f = map.getOrDefault(key, 0) + 1;
-                map.put(key,f);
-                maxF = Math.max(maxF, f);
-
+                map.put(key, map.getOrDefault(key, 0) + 1);
+                max = Math.max(max, map.getOrDefault(key, 0));
             }
-            answer = Math.max(answer, maxF + 1);
+            answer = Math.max(max+1, answer);
         }
-
         return answer;
     }
 
     public int gcd(int a, int b) {
         while (b != 0) {
             int temp = b;
-            b = a% b;
+            b = a % b;
             a = temp;
         }
+
         return a;
     }
 }
