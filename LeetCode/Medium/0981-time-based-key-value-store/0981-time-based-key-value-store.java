9e5ab08237bd17f47921f185a10f3b786f5e5f17
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-21 13:16:42 +0530

    Time: 129 ms (90.73%), Space: 108.3 MB (62.65%) - LeetHub

--- LeetCode/Medium/0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ LeetCode/Medium/0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -11,42 +11,36 @@ class TimeMap {
     }
 
     public String get(String key, int timestamp) {
-        List<Pair> list = map.get(key);
-        if(list==null)return "";
-        int left = 0;
-        int right = list.size() - 1;
+        List<Pair> pairs = map.get(key);
         Pair answer = null;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
 
-            if (list.get(mid).getTimestamp() <= timestamp) {
-                answer = list.get(mid);
+        if(pairs==null)return ""; 
+        int left = 0;
+        int right = pairs.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (pairs.get(mid).timestamp <= timestamp) {
+                answer = pairs.get(mid);
                 left = mid + 1;
-            } else
+            } else {
                 right = mid - 1;
+            }
         }
-        return answer!=null?answer.getValue():"";
+        return answer == null ? "" : answer.value;
     }
 }
 
 class Pair {
-    private int timestamp;
-    private String value;
+    int timestamp;
+    String value;
 
     public Pair(int timestamp, String value) {
         this.timestamp = timestamp;
         this.value = value;
     }
-
-    public int getTimestamp() {
-        return timestamp;
-    }
-
-    public String getValue() {
-        return value;
-    }
 }
-
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
