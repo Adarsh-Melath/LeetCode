f95af2770359a39dd091658fff40ad63dabb7cba
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-25 07:54:31 +0530

    Time: 6 ms (99.4%), Space: 49.5 MB (72.2%) - LeetHub

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map=new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        for(int i=0;i<strs.length;i++)
-        {
-            String word=strs[i];
-            char[] chars=word.toCharArray();
-            Arrays.sort(chars);
-            String sorted=new String(chars);
-
-            map.computeIfAbsent(sorted,k-> new ArrayList<>()).add(word);
+        for (String string : strs) {
+            char[] chs = string.toCharArray();
+            Arrays.sort(chs);
+            String key = new String(chs);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(string);
         }
-
         return new ArrayList<>(map.values());
     }
 }
