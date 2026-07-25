d7bcb49a0b05932b0799c7dd68d025fe59fe0346
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-25 08:54:33 +0530

    Time: 23 ms (83.04%), Space: 54.7 MB (12.95%) - LeetHub

--- LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ LeetCode/Medium/0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -4,15 +4,13 @@ class Solution {
         map.put(0, 1);
         int prefix = 0;
         int answer = 0;
-
         for (int num : nums) {
             prefix += num;
-
-            int remainder = ((prefix % k) + k)%k ;
-
-            answer += map.getOrDefault(remainder, 0) ;
+            int remainder = ((prefix % k) + k) % k;
+            answer += map.getOrDefault(remainder, 0);
             map.put(remainder, map.getOrDefault(remainder, 0) + 1);
         }
+
         return answer;
     }
 }
