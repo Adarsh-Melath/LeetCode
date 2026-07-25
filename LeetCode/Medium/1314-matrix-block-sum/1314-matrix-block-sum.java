b365451e5d40462afc7623ba1b3c177aac20b1f4
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-25 09:31:19 +0530

    Time: 10 ms (35.41%), Space: 47.2 MB (7.57%) - LeetHub

--- LeetCode/Medium/1314-matrix-block-sum/1314-matrix-block-sum.java
+++ LeetCode/Medium/1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -1,28 +1,32 @@
 class Solution {
-    public int[][] matrixBlockSum(int[][] matrix, int k) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int rows = mat.length;
+        int cols = mat[0].length;
 
-        int prefix[][] = new int[rows + 1][cols + 1];
-
+        int[][] prefix = new int[rows + 1][cols + 1];
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= cols; j++) {
-                prefix[i][j] = matrix[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+                prefix[i][j] = mat[i - 1][j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
             }
         }
 
+        System.out.println(Arrays.deepToString(prefix));
+
         int[][] answer = new int[rows][cols];
+
         for (int i = 0; i < rows; i++) {
             int row1 = Math.max(0, i - k);
             int row2 = Math.min(rows - 1, i + k);
+
             for (int j = 0; j < cols; j++) {
                 int col1 = Math.max(0, j - k);
                 int col2 = Math.min(cols - 1, j + k);
 
-                answer[i][j] = prefix[row2 + 1][col2 + 1] - prefix[row2 + 1][col1] - prefix[row1][col2 + 1]
+                answer[i][j] = prefix[row2 + 1][col2 + 1] -prefix[row2 + 1][col1] - prefix[row1][col2 + 1]
                         + prefix[row1][col1];
             }
         }
+
         return answer;
     }
 }
