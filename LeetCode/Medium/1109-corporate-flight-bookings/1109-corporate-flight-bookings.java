a6fad4c8c5810c4c9e4770094057eb972c33542d
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-28 12:42:34 +0530

    Time: 4 ms (36.91%), Space: 84.4 MB (84.71%) - LeetHub

--- LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ LeetCode/Medium/1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,21 +1,20 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] diff = new int[n + 1];
-        for (int[] booking : bookings) {
+        int[] diff = new int[n+1];
+        for (int booking[] : bookings) {
             int first = booking[0] - 1;
             int last = booking[1] - 1;
             int seats = booking[2];
 
             diff[first] += seats;
-            diff[last+1] -= seats;
+            diff[last + 1] -= seats;
         }
 
-        int[] answer = new int[n];
-        int currentPassengers = 0;
-
-        for (int i = 0; i < n; i++) {
-            currentPassengers += diff[i];
-            answer[i] = currentPassengers;
+        int answer[] = new int[n];
+        int currentSeats = 0;
+        for (int i = 0; i<n; i++) {
+            currentSeats += diff[i];
+            answer[i] = currentSeats;
         }
 
         return answer;
