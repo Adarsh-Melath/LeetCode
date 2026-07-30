bfcafad848bc30c3c5558ae00f8e38ef1d100be2
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-30 12:08:11 +0530

    Time: 24 ms (53.71%), Space: 65 MB (94.2%) - LeetHub

--- LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
+++ LeetCode/Medium/0525-contiguous-array/0525-contiguous-array.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
+
         map.put(0, -1);
         int prefix = 0;
         int answer = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                prefix--;
-            } else {
+            if (nums[i] == 1) {
                 prefix++;
+            } else {
+                prefix--;
             }
 
-            if(map.containsKey(prefix)){
-                int firstIndex=map.get(prefix);
-                answer=Math.max(answer,i-firstIndex);
-            }else{
-                map.put(prefix,i);
+            if (map.containsKey(prefix)) {
+                int previousIndex = map.get(prefix);
+                answer = Math.max(i-previousIndex, answer);
+            } else {
+                map.put(prefix, i);
             }
         }
-
         return answer;
     }
 }
