df69e37cd9f14da75cdaceace689c676e30b0ee7
Adarsh Melath <adarshmelath1305@gmail.com>
2026-07-31 12:01:12 +0530

    Time: 12 ms (6.3%), Space: 46.3 MB (62.82%) - LeetHub

--- LeetCode/Medium/1094-car-pooling/1094-car-pooling.java
+++ LeetCode/Medium/1094-car-pooling/1094-car-pooling.java
@@ -2,23 +2,24 @@ class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int[] diff = new int[1001];
 
-        for (int i = 0; i < trips.length; i++) {
-            int from = trips[i][1];
-            int to = trips[i][2];
-            int passengers = trips[i][0];
+        for (int[] trip : trips) {
+            int from = trip[1];
+            int to = trip[2];
+            int numPassengers = trip[0];
 
-            diff[from] += passengers;
-            diff[to] -= passengers;
+            diff[from] += numPassengers;
+            diff[to] -= numPassengers;
         }
+
+        System.out.println(Arrays.toString(diff));
 
         int passengers = 0;
         for (int i = 0; i < diff.length; i++) {
             passengers += diff[i];
-
-            if (passengers > capacity) {
+            if (passengers > capacity)
                 return false;
-            }
         }
+
         return true;
     }
 }
