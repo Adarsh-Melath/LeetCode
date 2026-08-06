613cbb0772aa82adffde3aa1a35f136780432de3
Adarsh Melath <adarshmelath1305@gmail.com>
2026-08-06 11:06:01 +0530

    Time: 27 ms (91.62%), Space: 73 MB (98.43%) - LeetHub

---
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxNonOverlapping(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(0);
+        int prefix = 0;
+        int answer = 0;
+        for (int num : nums) {
+            prefix += num;
+
+            int needed = prefix - target;
+
+            if (set.contains(needed)) {
+                answer++;
+                set.clear();
+            }
+            set.add(prefix);
+        }
+
+        return answer++;
+    }
+}
