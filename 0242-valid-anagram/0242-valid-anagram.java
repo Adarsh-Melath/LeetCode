d308b2a115ff5367455cf18c4f678ec4081c9f0c
Adarsh Melath <adarshmelath1305@gmail.com>
2026-08-19 17:41:41 +0530

    Time: 6 ms (43.34%), Space: 44.2 MB (89.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,15 +5,12 @@ class Solution {
         int[] hash = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
-            hash[s.charAt(i) - 'a']++;
+            hash[s.charAt(i)-'a']++;
+            hash[t.charAt(i)-'a']--;
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            hash[t.charAt(i) - 'a']--;
-        }
-
-        for (int i : hash) {
-            if (i != 0)
+        for (int count : hash) {
+            if (count != 0)
                 return false;
         }
 
