21655215950da49c317051b133a39e6f24c88e8f
Adarsh Melath <adarshmelath1305@gmail.com>
2026-08-25 11:47:07 +0530

    Time: 1 ms (100%), Space: 47.1 MB (76.34%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,16 +1,7 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        // int[] result = new int[nums.length];
 
-        // for (int i = 0; i < nums.length; i++) {
-        //     result[i] = nums[i] * nums[i];
-        // }
-
-        // Arrays.sort(result);
-
-        // return result;
-
-        int n=nums.length;
+        int n = nums.length;
         int[] result = new int[n];
 
         for (int i = 0; i < n; i++) {
@@ -23,20 +14,20 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (smallest < nums[i]) {
                 breakPoint = i;
+                break;
             }
         }
 
-        int writer=n-1;
-        int reader1=0;
-        int reader2=n-1;
+        int writer = n - 1;
+        int reader1 = 0;
+        int reader2 = n - 1;
 
-        while(reader1<=reader2)
-        {
-            if(nums[reader1]>nums[reader2]){
-                result[writer]=nums[reader1];
+        while (reader1 <= reader2) {
+            if (nums[reader1] > nums[reader2]) {
+                result[writer] = nums[reader1];
                 reader1++;
-            }else{
-                result[writer]=nums[reader2];
+            } else {
+                result[writer] = nums[reader2];
                 reader2--;
             }
 
