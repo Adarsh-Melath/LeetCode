be146763213d4e732b11d6cb948fb168ccce238f
Adarsh Melath <adarshmelath1305@gmail.com>
2026-08-27 12:50:28 +0530

    Time: 9 ms (8.89%), Space: 46.2 MB (61.18%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,32 +1,58 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int highest = Integer.MIN_VALUE;
-        for (int trip[] : trips) {
-            int from = trip[1];
-            int to = trip[2];
-
-            highest = Math.max(Math.max(from, to), highest);
-        }
-        int[] diff = new int[highest+1];
 
         for (int trip[] : trips) {
-            int passengers = trip[0];
-            int from = trip[1];
             int to = trip[2];
-
-            diff[from] += passengers;
-            diff[to] -= passengers;
+            highest = Math.max(to, highest);
         }
 
-        int currentPassengers = 0;
+        for (int f = 0; f <= highest; f++) {
+            int currentPassengers = 0;
 
-        for (int i = 0; i < diff.length; i++) {
-            currentPassengers += diff[i];
+            for (int[] trip : trips) {
+                int passengers = trip[0];
+                int from = trip[1];
+                int to = trip[2];
 
-            if (currentPassengers > capacity)
+                if (from <= f && f < to) {
+                    currentPassengers += passengers;
+                }
+            }
+
+            if (currentPassengers > capacity) {
                 return false;
+            }
         }
 
         return true;
+        // int highest = Integer.MIN_VALUE;
+
+        // for (int trip[] : trips) {
+        //     int to = trip[2];
+        //     highest = Math.max(to, highest);
+        // }
+
+        // int[] diff = new int[highest + 1];
+
+        // for (int trip[] : trips) {
+        //     int passengers = trip[0];
+        //     int from = trip[1];
+        //     int to = trip[2];
+
+        //     diff[from] += passengers;
+        //     diff[to] -= passengers;
+        // }
+
+        // int currentPassengers = 0;
+
+        // for (int i = 0; i < diff.length; i++) {
+        //     currentPassengers += diff[i];
+
+        //     if (currentPassengers > capacity)
+        //         return false;
+        // }
+
+        // return true;
     }
 }
