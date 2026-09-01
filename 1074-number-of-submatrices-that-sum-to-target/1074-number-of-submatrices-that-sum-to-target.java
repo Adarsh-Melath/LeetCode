5bf128b07ab49388bc31ff9bbb958ffab3b7fcd0
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-01 23:02:37 +0530

    Time: 127 ms (82.98%), Space: 47.3 MB (32.89%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -3,25 +3,38 @@ class Solution {
         int count = 0;
         int rows = matrix.length;
         int cols = matrix[0].length;
-        int[][] prefix_sum = new int[rows + 1][cols + 1];
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                prefix_sum[i][j]=matrix[i-1][j-1]+prefix_sum[i][j-1]+prefix_sum[i-1][j]-prefix_sum[i-1][j-1];
-            }
-        }
-        for (int r1 = 0; r1 < rows; r1++) {
-            for (int c1 = 0; c1 < cols; c1++) {
-                for (int r2 = r1; r2 < rows; r2++) {
-                    for (int c2 = c1; c2 < cols; c2++) {
-                        int sum = prefix_sum[r2+1][c2+1]+prefix_sum[r1][c1]-prefix_sum[r2+1][c1]-prefix_sum[r1][c2+1];
 
-                        if (sum == target) {
-                            count++;
-                        }
-                    }
+        for (int r1 = 0; r1 < rows; r1++) {
+            int[] col = new int[cols];
+            for (int r2 = r1; r2 < rows; r2++) {
+                for (int c1 = 0; c1 < cols; c1++) {
+                    col[c1] += matrix[r2][c1];
                 }
+
+                count += subarraySum(col, target);
             }
         }
         return count;
+    }
+
+    private int subarraySum(int[] col, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int prefix = 0;
+        int answer = 0;
+
+        for (int num : col) {
+            prefix += num;
+
+            int needed = prefix - target;
+
+            answer += map.getOrDefault(needed, 0);
+
+            map.put(prefix,map.getOrDefault(prefix,0)+1);
+        }
+
+        return answer;
     }
 }
