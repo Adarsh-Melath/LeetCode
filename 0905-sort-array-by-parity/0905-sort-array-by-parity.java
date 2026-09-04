3e557bac913196d95d7bca32040a791dda473a22
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-04 10:35:52 +0530

    Time: 1 ms (46%), Space: 46.7 MB (84.99%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,27 +1,51 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        ArrayList<Integer> even = new ArrayList<>();
-        ArrayList<Integer> odd = new ArrayList<>();
+        //brute O(n) O(n);
+        // ArrayList<Integer> even = new ArrayList<>();
+        // ArrayList<Integer> odd = new ArrayList<>();
 
-        for (int num : nums) {
-            if (num % 2 == 0) {
-                even.add(num);
+        // for (int num : nums) {
+        //     if (num % 2 == 0) {
+        //         even.add(num);
+        //     } else {
+        //         odd.add(num);
+        //     }
+        // }
+
+        // for (int i = 0; i < even.size(); i++) {
+        //     nums[i] = even.get(i);
+        // }
+
+        // for (int i = even.size(); i < nums.length; i++) {
+        //     nums[i] = odd.get(i - even.size());
+        // }
+
+        // return nums;
+
+        //optimal : O(n) O(n)
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] % 2 == 0) {
+                swap(nums, low, mid);
+
+                low++;
+                mid++;
             } else {
-                odd.add(num);
+                swap(nums, mid, high);
+                high--;
             }
         }
 
-        int[] result = new int[nums.length];
-
-        for (int i = 0; i < even.size(); i++) {
-            nums[i] = even.get(i);
-        }
-
-        for (int i = even.size(); i < nums.length; i++) {
-            nums[i] = odd.get(i - even.size());
-        }
-
         return nums;
+    }
 
+    private void swap(int nums[], int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
