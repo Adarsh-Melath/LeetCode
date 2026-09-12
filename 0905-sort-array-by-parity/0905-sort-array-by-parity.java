91b1201f274ffbe130347052c68f07255eb3d01d
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-12 10:53:47 +0530

    Time: 0 ms (100%), Space: 46.9 MB (49.81%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int left = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                swap(nums, left, i);
-                left++;
-            }
-        }
-        return nums;
-    }
+        int right = 0;
 
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        while (right<nums.length) {
+            if (nums[right] % 2 == 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left++] = temp;
+            }
+
+            right++;
+        }
+
+        return nums;
     }
 }
