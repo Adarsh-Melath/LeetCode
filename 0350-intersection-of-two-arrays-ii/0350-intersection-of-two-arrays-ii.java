6f00fd07d4cd2e8615b4de6a886787f772d6bcd3
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-12 10:15:49 +0530

    Time: 6 ms (32.43%), Space: 44.6 MB (89.89%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,26 +1,40 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> numbers1 = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : nums1) {
-            numbers1.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        List<Integer> intersections = new ArrayList<>();
-        for (int num:nums2) {
-            if (numbers1.contains(num)) {
-                intersections.add(num);
-                numbers1.remove(Integer.valueOf(num));
+        List<Integer> intersection = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                intersection.add(num);
+                map.computeIfPresent(num,(key,val)->val>1?val-1:null);
             }
         }
 
-        int[] result = new int[intersections.size()];
+        return intersection.stream().mapToInt(i->i).toArray();
+        // List<Integer> numbers1 = new ArrayList<>();
 
-        int index = 0;
-        for (int num : intersections) {
-            result[index++] = num;
-        }
+        // for (int num : nums1) {
+        //     numbers1.add(num);
+        // }
 
-        return result;
+        // List<Integer> intersections = new ArrayList<>();
+        // for (int num:nums2) {
+        //     if (numbers1.contains(num)) {
+        //         intersections.add(num);
+        //         numbers1.remove(Integer.valueOf(num));
+        //     }
+        // }
+
+        // int[] result = new int[intersections.size()];
+
+        // int index = 0;
+        // for (int num : intersections) {
+        //     result[index++] = num;
+        // }
+
+        // return result;
     }
 }
