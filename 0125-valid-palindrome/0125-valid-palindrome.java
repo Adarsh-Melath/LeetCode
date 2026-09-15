c1ba6bce96c409a0f57d7caac741e902d0ed51b2
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-15 22:14:30 +0530

    Time: 2 ms (99.29%), Space: 44.1 MB (82.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,18 +4,16 @@ class Solution {
         int right = s.length() - 1;
 
         while (left < right) {
-            if (!Character.isLetterOrDigit(s.charAt(left))) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
-            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            } else {
-                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
-                    return false;
-                }
-                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
-
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+                return false;
+            left++;
+            right--;
         }
 
         return true;
