3c2f28147d95115e7aff480816a99f255f46d270
Adarsh Melath <adarshmelath1305@gmail.com>
2026-09-17 22:44:46 +0530

    Time: 19 ms (81.85%), Space: 46.1 MB (20.88%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -23,13 +23,12 @@ class Solution {
                     } else if (sum == target) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
 
-                        left++;
-                        // right--;
-
-                        while (left < right && nums[left] == nums[left - 1])
+                        while (left < right && nums[left] == nums[left + 1])
                             left++;
                         while (left < right && nums[right] == nums[right - 1])
                             right--;
+                        left++;
+                        right--;
                     } else {
                         right--;
                     }
